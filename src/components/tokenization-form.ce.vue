51b391762ced823e70217c53e9da9b4ac8283bcd
gino caputo <gino@scaleit.team>
2024-03-20 03:21:52 -0300

    feat: adding shadow part for customize component

--- src/components/tokenization-form.ce.vue
+++ src/components/tokenization-form.ce.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import { computed, onMounted, reactive, ref } from 'vue';
-import { loadVGSCollect } from '@vgs/collect-js';
+import { computed, onMounted, reactive, ref } from "vue";
+import { loadVGSCollect } from "@vgs/collect-js";
 
 let form = ref({} as any);
 let formState = reactive({
   CreditCardNumber: { errorMessages: [] },
   Cvv: { errorMessages: [] },
   ExpirationDate: { errorMessages: [] },
-  CardHolderName: { errorMessages: [] }
+  CardHolderName: { errorMessages: [] },
 });
 const errorsVisible = ref(false);
-const submitButtonText = ref('Submit');
+const submitButtonText = ref("Submit");
 const isButtonDisabled = ref(false);
-const emit = defineEmits(['submit-success', 'validation-error']);
+const emit = defineEmits(["submit-success", "validation-error"]);
 
 const validateForm = (event: { preventDefault: () => void }) => {
   event.preventDefault();
@@ -25,11 +25,11 @@ const validateForm = (event: { preventDefault: () => void }) => {
 
   if (errorsVisible.value) {
     // Display errors and keep the button enabled
-    submitButtonText.value = 'Submit';
+    submitButtonText.value = "Submit";
     isButtonDisabled.value = false;
   } else {
     // No errors, proceed to tokenize
-    submitButtonText.value = 'Tokenizing...';
+    submitButtonText.value = "Tokenizing...";
     isButtonDisabled.value = true;
     submitForm();
     // Call your tokenization function here
@@ -37,133 +37,144 @@ const validateForm = (event: { preventDefault: () => void }) => {
 };
 onMounted(async () => {
   const collect: any = await loadVGSCollect({
-    vaultId: 'tntne5koztu',
-    environment: 'sandbox',
-    version: '2.7.0'
-  }).catch(e => {});
+    vaultId: "tntne5koztu",
+    environment: "sandbox",
+    version: "2.7.0",
+  }).catch((e) => {});
   form = collect.init((state: any) => {
-    if (state.CreditCardNumber && state.Cvv && state.ExpirationDate && state.CardHolderName)
+    if (
+      state.CreditCardNumber &&
+      state.Cvv &&
+      state.ExpirationDate &&
+      state.CardHolderName
+    )
       formState = state;
   });
   // Create JUPITER Collect field for credit card name
-  form.field('#cc-name', {
-    type: 'text',
-    name: 'CardHolderName',
-    placeholder: 'Joe Business',
-    validations: ['required']
+  form.field("#cc-name", {
+    type: "text",
+    name: "CardHolderName",
+    placeholder: "Joe Business",
+    validations: ["required"],
   });
 
   // Create JUPITER Collect field for credit card number
-  form.field('#cc-number', {
-    type: 'card-number',
-    name: 'CreditCardNumber',
-    successColor: '#4F8A10',
-    errorColor: '#D8000C',
-    placeholder: '4111 1111 1111 1111',
-    validations: ['required', 'validCardNumber']
+  form.field("#cc-number", {
+    type: "card-number",
+    name: "CreditCardNumber",
+    successColor: "#4F8A10",
+    errorColor: "#D8000C",
+    placeholder: "4111 1111 1111 1111",
+    validations: ["required", "validCardNumber"],
   });
 
   // Create JUPITER Collect field for CVC
-  form.field('#cc-cvc', {
-    type: 'card-security-code',
-    name: 'Cvv',
-    placeholder: '344',
-    validations: ['required', 'validCardSecurityCode']
+  form.field("#cc-cvc", {
+    type: "card-security-code",
+    name: "Cvv",
+    placeholder: "344",
+    validations: ["required", "validCardSecurityCode"],
   });
 
   // Create JUPITER Collect field for credit card expiration date
-  form.field('#cc-expiration-date', {
-    type: 'card-expiration-date',
+  form.field("#cc-expiration-date", {
+    type: "card-expiration-date",
     yearLength: 2,
-    name: 'ExpirationDate',
-    placeholder: '01 / 25',
+    name: "ExpirationDate",
+    placeholder: "01 / 25",
     serializers: [
       {
-        name: 'separate',
+        name: "separate",
         options: {
-          monthName: 'ExpirationMonth',
-          yearName: 'ExpirationYear'
-        }
-      }
+          monthName: "ExpirationMonth",
+          yearName: "ExpirationYear",
+        },
+      },
     ],
-    validations: ['required', 'validCardExpirationDate']
+    validations: ["required", "validCardExpirationDate"],
   });
-  form.value = form
+  form.value = form;
 });
 // Handle form submission
 const submitForm = async () => {
   try {
     // Implement the form submission logic here
-    const env = 'sandbox-platform';
-    const subMerchantId = 'jpt-sim-md-1';
+    const env = "sandbox-platform";
+    const subMerchantId = "jpt-sim-md-1";
     let resp = await fetch(
-      'https://' +
+      "https://" +
         env +
-        '.jupiterhq.com/v1/transactions/creditcard/tokenization/' +
+        ".jupiterhq.com/v1/transactions/creditcard/tokenization/" +
         subMerchantId +
-        '/session'
+        "/session"
     );
     let sessionData = await resp.json();
     let sessionId;
-    if (sessionData.data && sessionData.data.status === 'active') {
+    if (sessionData.data && sessionData.data.status === "active") {
       sessionId = sessionData.data.sessionId;
     }
-    if (form.value) { // Accediendo a la instancia de VGSCollect a través de .value
+    if (form.value) {
+      // Accediendo a la instancia de VGSCollect a través de .value
 
-    //submit cc fields to VGS for tokenization
-    form.value.submit(
-      '/v1/transactions/creditcard/tokenization/' + subMerchantId,
-      { data: { sessionToken: sessionId } },
-      async (status: any, response:any) => {
-        submitButtonText.value = 'Submit';
-        emit('submit-success', { data: response.data.token });
-        submitButtonText.value = 'Submit';
-        isButtonDisabled.value = false;
-      }
-    );
+      //submit cc fields to VGS for tokenization
+      form.value.submit(
+        "/v1/transactions/creditcard/tokenization/" + subMerchantId,
+        { data: { sessionToken: sessionId } },
+        async (status: any, response: any) => {
+          submitButtonText.value = "Submit";
+          emit("submit-success", { data: response.data.token });
+          submitButtonText.value = "Submit";
+          isButtonDisabled.value = false;
+        }
+      );
     }
   } catch (error) {
     // Handle or log the error
-    submitButtonText.value = 'Submit';
+    submitButtonText.value = "Submit";
     isButtonDisabled.value = false;
-    emit('validation-error', { message: 'Error de validación', formState });
+    emit("validation-error", { message: "Error de validación", formState });
 
     // You might also want to update the UI or state to reflect the error
   }
 };
-
 </script>
 
 <template>
   <main>
     <div class="row">
       <div class="col-md-12">
-        <div class="row card card-outline-secondary">
+        <div class="row card card-outline-secondary" part="card">
           <div class="card-body">
-            <div class="alert alert-info p-2">
-              Please fill in and submit a form to get the token.
-            </div>
             <form id="cc-form">
-              <div class="form-group">
-                <label for="cc-name">Name</label>
-                <slot name="cc-name"></slot>
+              <div part="cc-name">
+                <div class="form-group">
+                  <label for="cc-name">Name</label>
+                  <slot name="cc-name"></slot>
+                </div>
               </div>
-              <div class="form-group">
-                <label for="cc-number">Card number</label>
-                <slot name="cc-number"></slot>
+              <div part="cc-number">
+                <div class="form-group">
+                  <label for="cc-number">Card number</label>
+                  <slot name="cc-number"></slot>
+                </div>
               </div>
-              <div class="form-group">
-                <label for="cc-cvc">CVC</label>
-                <slot name="cc-cvc"></slot>
+              <div part="cc-expiration-date">
+                <div class="form-group">
+                  <label for="cc-expiration-date">Expiration</label>
+                  <slot name="cc-expiration-date"></slot>
+                </div>
               </div>
-              <div class="form-group">
-                <label for="cc-expiration-date">Expiration date</label>
-                <slot name="cc-expiration-date"></slot>
+              <div part="cc-cvc">
+                <div class="form-group">
+                  <label for="cc-cvc">CVC</label>
+                  <slot name="cc-cvc"></slot>
+                </div>
               </div>
 
               <!--Submit credit card form button-->
               <div>
                 <button
+                  part="button"
                   id="submitButton"
                   class="btn btn-success btn-block ml-auto"
                   @click="validateForm($event)"
@@ -180,13 +191,22 @@ const submitForm = async () => {
               >
                 Card number {{ formState.CreditCardNumber.errorMessages[0] }}
               </div>
-              <div class="error-msg" v-if="formState.Cvv.errorMessages.length > 0">
+              <div
+                class="error-msg"
+                v-if="formState.Cvv.errorMessages.length > 0"
+              >
                 CVC {{ formState.Cvv.errorMessages[0] }}
               </div>
-              <div class="error-msg" v-if="formState.ExpirationDate.errorMessages.length > 0">
+              <div
+                class="error-msg"
+                v-if="formState.ExpirationDate.errorMessages.length > 0"
+              >
                 Expiration date {{ formState.ExpirationDate.errorMessages[0] }}
               </div>
-              <div class="error-msg" v-if="formState.CardHolderName.errorMessages.length > 0">
+              <div
+                class="error-msg"
+                v-if="formState.CardHolderName.errorMessages.length > 0"
+              >
                 Name {{ formState.CardHolderName.errorMessages[0] }}
               </div>
             </div>
@@ -197,17 +217,16 @@ const submitForm = async () => {
   </main>
 </template>
 
-
 <style scoped>
-@import 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css';
+@import "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
 
-span[id*='cc-'] {
+span[id*="cc-"] {
   display: block;
   height: 40px;
   margin-bottom: 15px;
 }
 
-span[id*='cc-'] iframe {
+span[id*="cc-"] iframe {
   height: 100%;
   width: 100%;
 }
